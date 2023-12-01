7345e823de4ff987ac45da1b032c71634961f16c
Kyo Duy <khanhduya2hytq@gmail.com>
2023-12-02 00:56:38 +0700

    send mail redirect

--- be/src/main/java/com/module/project/controller/AuthenticationController.java
+++ be/src/main/java/com/module/project/controller/AuthenticationController.java
@@ -2,14 +2,13 @@ package com.module.project.controller;
 
 import com.module.project.dto.request.AuthenticationRequest;
 import com.module.project.dto.request.RegisterRequest;
-import com.module.project.dto.response.AuthenticationResponse;
 import com.module.project.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +21,8 @@ import static com.module.project.dto.Constant.AUTH;
 import static com.module.project.dto.Constant.AUTHENTICATE;
 import static com.module.project.dto.Constant.REGISTER;
 import static com.module.project.dto.Constant.VERIFY;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(API_V1 + AUTH)
@@ -38,12 +39,16 @@ public class AuthenticationController {
 
     @GetMapping(VERIFY)
     public ResponseEntity<Object> verify(@RequestParam(value = "username") String username) {
-        return ResponseEntity.ok(service.verify(username));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("http://localhost/hms/login"));
+        service.verify(username);
+        // Create a ResponseEntity with status code 302 and headers
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
     @PostMapping(AUTHENTICATE)
     public ResponseEntity<Object> authentication(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authentication(request));
     }
-
 }
